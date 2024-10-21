c9bbb5110ba151c8b664c5fa6b9ce20c12492c89
nekocheik <cheik.kone@hetic.net>
2024-10-21 23:07:17 +0200

    udpate

--- _frontend/src/nodes/index.ts
+++
@@ -1,8 +0,0 @@
-// my-low-code-app/frontend/src/nodes/index.ts
-
-import type { NodeTypes } from 'reactflow';
-import { PositionLoggerNode } from './PositionLoggerNode';
-
-export const nodeTypes = {
-  'position-logger': PositionLoggerNode,
-} satisfies NodeTypes;
--- _frontend/src/nodes/types.ts
+++
@@ -1,5 +0,0 @@
-import type { Node } from 'reactflow';
-
-export type PositionLoggerNode = Node<{ label: string }, 'position-logger'>;
-export type AppNode = PositionLoggerNode;
-// Ajoutez d'autres types de nœuds ici si nécessaire
